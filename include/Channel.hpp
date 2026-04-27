7b72c588d03b1e9e7bdf33b37dec253cab00ffb4
Hamza Benmohamd <hbenmoha@e2r13p1.1337.ma>
2026-04-27 16:07:47 +0100

    create the channel class

---
+++ include/Channel.hpp
@@ -0,0 +1,45 @@
+#ifndef CHANNEL_HPP
+# define CHANNEL_HPP
+
+# include <string>
+# include <map>
+# include "Client.hpp" // We need the Client class to store references to users
+
+class Channel
+{
+	private:
+		std::string                 _name;
+		std::string                 _topic;
+		std::string                 _key;             //? Password for the channel (mode k)
+		bool                        _inviteOnly;      //? Invite-only mode (mode i)
+		bool                        _topicRestricted; //? Restrict topic changes to ops (mode t)
+		size_t                      _userLimit;       //? Max users allowed (mode l)
+
+		// We use pointers (Client*) so we don't accidentally copy the user's data!
+		// We want to reference the exact same Client object that the Server is managing.
+		std::map<int, Client*>      _clients;         //? Map of regular users (Key: fd, Value: Client pointer)
+		std::map<int, Client*>      _operators;       //? Map of channel operators (Key: fd, Value: Client pointer)
+
+	public:
+		//* Constructor & Destructor *//
+		Channel(std::string name);
+		~Channel();
+
+		//*  Getters *//
+		std::string getName() const;
+
+		//* Client Management *//
+		void addClient(Client* client);
+		void removeClient(int fd);
+		bool isClientInChannel(int fd);
+
+		//* Operator Management *//
+		void addOperator(Client* client);
+		void removeOperator(int fd);
+		bool isOperator(int fd);
+
+		//* Core Action *//
+		void broadcastMessage(std::string msg, int senderFd);
+};
+
+#endif
