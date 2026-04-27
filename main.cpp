858fb39f7f02d832fcf9f591e16dc8bf8f7c2f5d
Hamza Benmohamd <hbenmoha@e2r13p1.1337.ma>
2026-04-27 16:10:03 +0100

    just make more readable, add new lines

--- main.cpp
+++ main.cpp
@@ -1,24 +1,24 @@
-
 #include "include/Server.hpp"
 
-
-
-int main(int ac, char **av){
-
-    if (ac != 3){
+int main(int argc, char const *argv[])
+{
+    if (argc != 3)
+	{
         std::cerr << "[ERROR]: ./ircserv <Port> <Password>" << std::endl;
         return (EXIT_FAILURE);
     }
 
-    int Port = std::atoi(av[1]);
-    std::string Password = av[2];
+    int Port = std::atoi(argv[1]);
+    std::string Password = argv[2];
 
-    if (Port <= MAX_SYS_PORT || Port > MAX_PORT){
+    if (Port <= MAX_SYS_PORT || Port > MAX_PORT)
+	{
         std::cerr << "[IRCSERV]: Invalid Port Number !!" << std::endl;
         return (EXIT_FAILURE);
     }
 
-    if (Password.empty()){
+    if (Password.empty())
+	{
         std::cerr << "[IRCSERV]: Password Cannot Be Empty !!" << std::endl;
         return (EXIT_FAILURE);
     }
